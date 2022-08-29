b2db4321e1486f9cd9b3ca889f3b98a15f2461e8
volodymyr kladochnyi <vladimir.kladochnyi@skylum.com>
2022-08-29 22:38:05 +0300

    added storybook

--- client/src/components/MoiveCard/index.js
+++ client/src/components/MoiveCard/index.js
@@ -1,41 +1,49 @@
-import React from 'react';
-import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import { styled } from '@mui/material/styles';
+import React from "react";
+import PropTypes from "prop-types";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
 
-import { CardMenu } from './components';
+import { CardMenu } from "./components";
 
-const CardInfo = styled(CardContent)(({theme}) => ({
-    '&:last-child': {
-        paddingBottom: theme.spacing(2),
-      }
-  }));
+const CardInfo = styled(CardContent)(({ theme }) => ({
+  "&:last-child": {
+    paddingBottom: theme.spacing(2),
+  },
+}));
 
+const MovieCard = ({ moive, onCardSelect }) => {
+  return (
+    <Card sx={{ maxWidth: 250, position: "relative" }}>
+      <CardMenu onCardSelect={onCardSelect} />
+      <CardMedia
+        component="img"
+        height="250"
+        image={moive.image}
+        alt={moive.title}
+      />
+      <CardInfo>
+        <Typography variant="h6" gutterBottom component="div">
+          {moive.title}
+        </Typography>
 
-const MovieCard = () => {
-    const onAddClick = (movie) => alert('movie is added')
-    return (
-        <Card sx={{ maxWidth: 250, position: "relative" }}>
-            <CardMenu onAddClick={onAddClick}/>
-            <CardMedia
-                component="img"
-                height="250"
-                image="https://www.themoviedb.org/t/p/w440_and_h660_face/6DrHO1jr3qVrViUO6s6kFiAGM7.jpg"
-                alt="Paella dish"
-            />
-            <CardInfo>
-                <Typography variant="h6" gutterBottom component="div">
-                    Sonic the Hedgehog 2
-                </Typography>
+        <Typography mb={0} variant="subtitle1" gutterBottom component="div">
+          {moive.releaseDate}
+        </Typography>
+      </CardInfo>
+    </Card>
+  );
+};
 
-                <Typography mb={0} variant="subtitle1" gutterBottom component="div">
-                    Apr 08, 2022
-                </Typography>
-            </CardInfo>
-        </Card>
-    )
-}
+MovieCard.propTypes = {
+  moive: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    releaseDate: PropTypes.string
+  }).isRequired,
+  onCardSelect: PropTypes.func
+};
 
-export default MovieCard; 
+export default MovieCard;
---
+++ client/src/stories/stub.js
@@ -0,0 +1,44 @@
+export const moives = [
+  {
+    _id: "630d13b3ec79a9f3b50074ee",
+    image: "https://www.themoviedb.org/t/p/w440_and_h660_face/3ikDvCILbB49L8uQmGcPK8hrKQw.jpg",
+    title: "Entality",
+    releaseDate: "2016-10-16T12:22:42 -03:00",
+  },
+  {
+    _id: "630d13b336ef2e98e968b6ae",
+    image: "https://www.themoviedb.org/t/p/w440_and_h660_face/3ikDvCILbB49L8uQmGcPK8hrKQw.jpg",
+    title: "Imant",
+    releaseDate: "2015-04-28T08:58:10 -03:00",
+  },
+  {
+    _id: "630d13b30fb32605e20094a2",
+    image: "https://www.themoviedb.org/t/p/w440_and_h660_face/3ikDvCILbB49L8uQmGcPK8hrKQw.jpg",
+    title: "Flotonic",
+    releaseDate: "2019-09-18T01:46:02 -03:00",
+  },
+  {
+    _id: "630d13b398dde0dbb27f96ce",
+    image: "https://www.themoviedb.org/t/p/w440_and_h660_face/3ikDvCILbB49L8uQmGcPK8hrKQw.jpg",
+    title: "Geostele",
+    releaseDate: "2020-04-21T08:29:55 -03:00",
+  },
+  {
+    _id: "630d13b350affad65c0a5882",
+    image: "https://www.themoviedb.org/t/p/w440_and_h660_face/3ikDvCILbB49L8uQmGcPK8hrKQw.jpg",
+    title: "Jimbies",
+    releaseDate: "2016-01-21T04:37:43 -02:00",
+  },
+  {
+    _id: "630d13b3efb2d498473dfe59",
+    image: "https://www.themoviedb.org/t/p/w440_and_h660_face/3ikDvCILbB49L8uQmGcPK8hrKQw.jpg",
+    title: "Twiist",
+    releaseDate: "2016-03-24T04:09:14 -02:00",
+  },
+  {
+    _id: "630d13b3985f5f3b611cfb28",
+    image: "https://www.themoviedb.org/t/p/w440_and_h660_face/3ikDvCILbB49L8uQmGcPK8hrKQw.jpg",
+    title: "Tribalog",
+    releaseDate: "2022-04-30T05:11:31 -03:00",
+  },
+];
